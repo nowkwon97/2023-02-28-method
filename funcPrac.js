63626088eb3a5b438e6864bb4e9b44589770bb5c
leejaekwon <nowkwon97@gmail.com>
2023-03-01 15:48:07 +0900

    구조 만드는 함수

--- funcPrac.js
+++ funcPrac.js
@@ -3,9 +3,11 @@ function makeTag(tagName, setAttributeName, setAttributeValue, parent) {
   const element = document.createElement(tagName);
   element.setAttribute(setAttributeName, setAttributeValue);
   parent.appendChild(element);
+  
 };
 
-function makeStructure(container, ) {
-  const element = document.getElementById(container);
-  
+// id 가 없는 구조를 생성하는 함수
+function makeStructure(container, tagName) {
+  const newTag = document.createElement(tagName);
+  container.appendChild(newTag);
 }
